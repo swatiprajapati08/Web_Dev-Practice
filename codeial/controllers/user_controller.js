83893b0ad959a5203e2f5a30cd8385c94d3bc1a7
Swati <prajapatiswati4@gmail.com>
2022-02-19 12:34:27 +0530

    added user signin and signup

--- codeial/controllers/user_controller.js
+++ codeial/controllers/user_controller.js
@@ -3,3 +3,26 @@ module.exports.profile = function(request,response){
         title:"users",
     });
 }
+
+module.exports.signUp = function(request,response){
+    return response.render('user_sign_up',{
+        title: "Codiel | Sign Up",
+    });
+};
+
+module.exports.signIn = function(request,response){
+    return response.render('user_sign_in',{
+        title : "Codiel | SIgn In",
+    });
+}
+
+//get the sign up data
+module.exports.create = function(request,response){
+    //TODO Later
+}
+
+
+//sign in and create a session for the user
+module.exports.createSession= function(request,response){
+    //TODO Later
+}
