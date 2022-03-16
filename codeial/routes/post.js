8bec62d835087ff46d054b900d746b681e0f2b14
Swati <prajapatiswati4@gmail.com>
2022-03-16 21:23:18 +0530

    added the checks only valid user can postthe content

--- codeial/routes/post.js
+++ codeial/routes/post.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const postController = require('../controllers/post_controller');
 
-router.post('/create',postController.create);
+// user is authenticate then only post the content
+router.post('/create',passport.checkAuthentication, postController.create);
 
 module.exports = router;
