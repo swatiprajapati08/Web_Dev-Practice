a733dccabdb1b402a83ab55098062829df0b65cc
Swati <prajapatiswati4@gmail.com>
2022-04-01 23:52:06 +0530

    Deleting a post by authorised user

--- codeial/controllers/post_controller.js
+++ codeial/controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comments');
 module.exports.create = function(request,response){
     Post.create({
         content: request.body.content,
@@ -9,5 +10,25 @@ module.exports.create = function(request,response){
             return;
         }
         return response.redirect('back');
+    });
+}
+
+
+module.exports.destroy = function(request,response){
+    Post.findById(request.params.id,function(error,post){
+        // user is authorised or not(neans user which created that post)
+
+        //.id means converting the objct into the string
+        if(post.user == request.user.id){
+            post.remove();
+            Comment.deleteMany(
+                {post:request.params.id},function(error){
+                    return response.redirect('back');
+                }
+            );
+        }
+        else{
+            return response.redirect('back');
+        }
     });
 }
--- codeial/routes/post.js
+++ codeial/routes/post.js
@@ -5,5 +5,5 @@ const postController = require('../controllers/post_controller');
 
 // user is authenticate then only post the content
 router.post('/create',passport.checkAuthentication, postController.create);
-
+router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
 module.exports = router;
